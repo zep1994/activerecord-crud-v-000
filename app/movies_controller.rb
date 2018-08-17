28898d57c875bd9f519289b5bacdd0936e4951c2
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-17 19:47:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,7 +38,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.count
+  Movie.count.title
 end
 
 def can_find_the_first_item_from_the_database_using_id
